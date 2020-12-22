18a5a5992c9819c7e42c3ccccc97b9fbb9e3088c
Akin Aguda <akin@kanda.co.uk>
2020-12-23 00:20:54 +0100

    correctly implemnted de casteljau's algorithm

--- src/classes/Bezier.ts
+++ src/classes/Bezier.ts
@@ -11,40 +11,13 @@ export enum PointType {
 export default class Bezier {
     constructor(public graph: Graph) {}
     controlPoints: Array<Point>;
-    // indicate() {
-    //     this.graph.arm.forEach(point => point.drawCircle(this.graph.ctx))
-    // }
-
-    // showControlPoints() {
-    //     this.controlPoints.forEach(point => point.drawCircle(this.graph.ctx))
-    // }
-
-    // genF(upperBoundary: number, lowerBoundary: number, iteration: number, axi: number) {
-    //     return combination(upperBoundary, lowerBoundary) * Math.pow((1 - iteration), upperBoundary - lowerBoundary) * Math.pow(iteration, lowerBoundary) * axi
-    // }
-
-    // summation(upperBoundary: number, lowerBoundary: number, iteration: number, points: Array<Point>, pType: PointType): number {
-    //     if (lowerBoundary === upperBoundary) {
-    //         return this.genF(upperBoundary, lowerBoundary, iteration, returnAxi(pType, points[lowerBoundary - 1]))
-    //     } else {
-    //         return this.genF(upperBoundary, lowerBoundary, iteration, returnAxi(pType, points[lowerBoundary])) + this.summation(upperBoundary, lowerBoundary + 1, iteration, points, pType)
-    //     }
-    // }
 
     calculateValue(n: number, i: number, t: number, axi: number, ty: PointType) : number {
-        if (t === 0 && ty == PointType.y) {
-            // console.log(combination(n, i) * (Math.pow((i - t), n - 1)) * Math.pow(t, i) * axi, axi)
-            console.log(t, n, i, axi, (Math.pow((1 - t), n - 1)), combination(n, i) * (Math.pow((i - t), n - 1)) * Math.pow(t, i) * axi)
-        }
-        // console.log(t)
-        // console.log(n, i)
-       return combination(n, i) * (Math.pow((1 - t), n - 1)) * Math.pow(t, i) * axi 
+       return combination(n, i) * (Math.pow((1 - t), n - i)) * Math.pow(t, i) * axi 
     }
 
     summation(upperBoundary: number, lowerBoundary: number, iteration: number, points: Array<Point>, pType: PointType) : number {
-        // console.log()
         if (lowerBoundary === upperBoundary) {
-            // console.log("finished");
             return this.calculateValue(upperBoundary, lowerBoundary, iteration, returnAxi(pType, points[lowerBoundary]), pType)
         } else {
             return this.calculateValue(upperBoundary, lowerBoundary, iteration, returnAxi(pType, points[lowerBoundary]), pType) + this.summation(upperBoundary, lowerBoundary + 1, iteration, points, pType)
@@ -55,30 +28,12 @@ export default class Bezier {
         const x: Array<number> = [];
         const y: Array<number> = [];
         const pts: Array<Point> = [];
-        // this.controlPoints = controlPoints;
-        for (let i = 0; i < 1; i += 0.05) {
-            // console.log(i);
-            // x.push(this.summation(controlPoints.length - 1, 0, i, controlPoints, PointType.x ))
-            // y.push(this.summation(controlPoints.length - 1, 0, i, controlPoints, PointType.y ))
+        for (let i = 0; i <= 1; i = Math.round((i + 0.05 + Number.EPSILON) * 100) / 100) {
             pts.push(this.graph.point(this.summation(controlPoints.length - 1, 0, i, controlPoints, PointType.x ), this.summation(controlPoints.length - 1, 0, i, controlPoints, PointType.y )))
         }
-
-        // console.log(x)
-
-        // x.forEach((_, i) => {
-        //     console.log(x[i], y[i])
-        //     this.graph.point(x[i]/50, y[i]/50)
-        // })
-
         for (let i = 0; i < pts.length - 1; i++) {
             this.graph.line(pts[i], pts[i + 1])
-            // pts[i].drawCircle(this.graph.ctx)
         }
-
-        // console.log(pts[0])
-
-
-
     }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -6,57 +6,14 @@ const app = new App();
 
 import Graph from './classes/Graph';
 import Bezier from './classes/Bezier';
-import Point from './classes/Point';
 
-// const graph = new Graph({id: "myCanvas", height: 758, width: 135});
-
-// const draw = (p1: Point, p2: Point, p3: Point, p4: Point) => {
     const graph = new Graph({ id: "myCanvas" });
-    // const bezier = new Bezier(graph);
-    // bezier.buildBezier([graph.point(2, 1)]);
-    // // , new Point(450, 200), new Point(500, 250)]
-    // bezier.showControlPoints();
-    // console.log(bezier.controlPoints, graph.arm)
-    // bezier.indicate();
-    // bezier.buildBezier([p1, p2, p3, p4]);
-
-    // bezier.showControlPoints();
     const pnt1 = graph.point(1, 4);
     const pnt2 = graph.point(5, 6);
     const pnt3 = graph.point(5, 1);
-    // const pnt4 = graph.point(4, 2);
+
     const bezier = new Bezier(graph);
     bezier.buildBezier([pnt1, pnt2, pnt3]);
     pnt1.drawCircle(graph.ctx);
     pnt2.drawCircle(graph.ctx)
     pnt3.drawCircle(graph.ctx)
-    // pnt4.drawCircle(graph.ctx)
-// }
-
-    // draw(p1, p2, p3, p4)
-
-// const a = document.getElementById("a") as HTMLInputElement;
-// const b = document.getElementById("b") as HTMLInputElement;
-// const c = document.getElementById("c") as HTMLInputElement;
-// const d = document.getElementById("d") as HTMLInputElement;
-// const e = document.getElementById("e") as HTMLInputElement;
-// const f = document.getElementById("f") as HTMLInputElement;
-// const g = document.getElementById("g") as HTMLInputElement;
-// const h = document.getElementById("h") as HTMLInputElement;
-
-// const fromInputs = () => {
-//     const p1 = graph.point(Number(a.value), Number(b.value))
-//     const p2 = graph.point(Number(c.value), Number(d.value))
-//     const p3 = graph.point(Number(e.value), Number(f.value))
-//     const p4 = graph.point(Number(g.value), Number(h.value))
-
-//     draw(p1, p2, p3, p4)
-// }
-
-// [a, b, c, d, e, f, g, h].forEach(i => {
-//     i.addEventListener('input', fromInputs)
-// })
-
-// fromInputs();
-
-
