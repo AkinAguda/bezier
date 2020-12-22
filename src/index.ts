a47892415d02bec886cca6014f8ff0f61185c6b6
Akin Aguda <akin@kanda.co.uk>
2020-12-23 00:24:10 +0100

    constructed a cubic curve

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,11 @@ import Bezier from './classes/Bezier';
     const pnt1 = graph.point(1, 4);
     const pnt2 = graph.point(5, 6);
     const pnt3 = graph.point(5, 1);
+    const pnt4 = graph.point(8, 6);
 
     const bezier = new Bezier(graph);
-    bezier.buildBezier([pnt1, pnt2, pnt3]);
+    bezier.buildBezier([pnt1, pnt2, pnt3, pnt4]);
     pnt1.drawCircle(graph.ctx);
     pnt2.drawCircle(graph.ctx)
     pnt3.drawCircle(graph.ctx)
+    pnt4.drawCircle(graph.ctx)
