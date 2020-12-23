f9dd5fd6cf869f4586047e37214c4b8f175b236e
Akin Aguda <akin@kanda.co.uk>
2020-12-23 09:04:19 +0100

    removed comments and formatted code a bit

--- src/classes/Graph.ts
+++ src/classes/Graph.ts
@@ -14,28 +14,19 @@ export default class Graph {
         this._ctx = this._canvas.getContext("2d");
         this._height = this._canvas.offsetHeight;
         this._width = this._canvas.offsetWidth;
-        // console.log(this._height / this._scale, this._width / this._scale)
         this._canvas.height = this._height;
         this._canvas.width = this._width;
     }
-    // line(p1: Point, p2: Point) {
-    //     this._ctx.moveTo(p1.x, this._height - p1.y);
-    //     this._ctx.lineTo(p2.x, this._height - p2.y);
-    //     this._ctx.stroke();
-    //     console.log("draw")
-    // }
+
     line(p1: Point, p2: Point) {
         this._ctx.moveTo(p1.rawX, p1.rawY);
         this._ctx.lineTo(p2.rawX, p2.rawY);
         this._ctx.strokeStyle = '#8EB0F5';
         this._ctx.stroke();
-        // console.log("Hi")
     }
     point(x: number, y: number): Point {
         const newPoint = new Point(x, y, (x * this._scale), this._height - (y * this._scale));
-        // console.log(this._height)
         this._ctx.moveTo(newPoint.rawX, newPoint.rawY);
-        // const newPoint = new Point(x, y, x, this._height - y);
         this.points.push(newPoint);
         return newPoint
     }
--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ const app = new App();
 import Graph from './classes/Graph';
 import Bezier from './classes/Bezier';
 
-    const graph = new Graph({ id: "myCanvas" });
-    const pnt1 = graph.point(1, 4);
-    const pnt2 = graph.point(5, 6);
-    const pnt3 = graph.point(5, 1);
-    const pnt4 = graph.point(8, 6);
+const graph = new Graph({ id: "myCanvas" });
+const pnt1 = graph.point(1, 4);
+const pnt2 = graph.point(5, 6);
+const pnt3 = graph.point(5, 1);
+const pnt4 = graph.point(8, 6);
 
-    const bezier = new Bezier(graph);
-    bezier.buildBezier([pnt1, pnt2, pnt3, pnt4]);
-    pnt1.drawCircle(graph.ctx);
-    pnt2.drawCircle(graph.ctx)
-    pnt3.drawCircle(graph.ctx)
-    pnt4.drawCircle(graph.ctx)
+const bezier = new Bezier(graph);
+bezier.buildBezier([pnt1, pnt2, pnt3, pnt4]);
+pnt1.drawCircle(graph.ctx);
+pnt2.drawCircle(graph.ctx)
+pnt3.drawCircle(graph.ctx)
+pnt4.drawCircle(graph.ctx)
