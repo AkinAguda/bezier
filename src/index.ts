2a1e097938a9db2a73427bcc47f48e1b47d6821e
Akin Aguda <akin@kanda.co.uk>
2021-01-04 01:44:32 +0100

    cleaned up a bit

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,4 @@ import App from './classes/App';
 
 import './styles/index.scss';
 
-const app = new App();
-
-// import Graph from './classes/Graph';
-// import Bezier from './classes/Bezier';
-
-// const graph = new Graph({ id: "myCanvas" });
-// const pnt1 = graph.point(4, 2);
-// const pnt2 = graph.point(7, 4);
-// const pnt3 = graph.point(9, 1);
-// const pnt4 = graph.point(13, 3);
-
-// const bezier = new Bezier(graph);
-// bezier.buildBezier([pnt1, pnt2, pnt3, pnt4]);
-// pnt1.drawCircle(graph.ctx);
-// pnt2.drawCircle(graph.ctx)
-// pnt3.drawCircle(graph.ctx)
-// pnt4.drawCircle(graph.ctx)
+new App();
